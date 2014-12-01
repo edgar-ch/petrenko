747b5ad26449c0b87bcc115c604a8f3eb3f63ddc
Edgar Cherkasov <edgar.cherkasov@yandex.ru>
2014-12-01 03:56:54 +0300

    Add tests and some fixes

---
+++ flex/lab3/test.c
@@ -0,0 +1,11 @@
+#include <stdafx.h>
+#define asb 100
+
+int main(int argc/*количество */,char* argv[])
+{
+	//printf("Hello C++\n");
+	//return -1;
+	printf/*функция печати*/("Hello World!\n");
+	return 0; // возврат программы
+	/* и еще один бессмысленный и беспощадный коммент */
+}
